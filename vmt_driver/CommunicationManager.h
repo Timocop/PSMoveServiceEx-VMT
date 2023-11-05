2cf14e0b369d093ef0219037ba4bdb5af14cf7d7
Externet <timocop@hotmail.de>
2023-11-05 21:54:29 +0100

    New Protocol Version

--- vmt_driver/CommunicationManager.h
+++ vmt_driver/CommunicationManager.h
@@ -26,7 +26,7 @@ SOFTWARE.
 
 //通信系の処理の管理、OSC情報の送受信を行う
 namespace VMTDriver {
-	const string Version = "VMTX_016";
+	const string Version = "VMTX_017";
 
 	class OSCReceiver : public osc::OscPacketListener {
 	private:
